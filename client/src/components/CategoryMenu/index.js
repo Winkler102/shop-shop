137e29936930aa04921041a0a73d564d0f6725b5
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-08-08 19:13:27 -0500

    added cart

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-import { useStoreContext } from "../../utils/GlobalState";
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
+
   const { categories } = state;
+
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
